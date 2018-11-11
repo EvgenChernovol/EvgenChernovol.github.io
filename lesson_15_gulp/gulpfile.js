efefcfad17ce8455d8c2e1974272a439d4ac931c
Evgen_Chernovol <ievgenchernovol@gmail.com>
2018-11-11 15:22:39 +0200

    exam2

--- lesson_15_gulp/gulpfile.js
+++ lesson_15_gulp/gulpfile.js
@@ -37,7 +37,8 @@ gulp.task('img', () => {
 
 gulp.task('watch', () => {
  gulp.watch('src/scss/**/*.scss', ['sass']),
- gulp.watch('src/index.html',['html'])
+ gulp.watch('src/index.html',['html']),
+ gulp.watch('src/img/**/*.*', ['img']),
 });
 
 gulp.task('connect', function() {
