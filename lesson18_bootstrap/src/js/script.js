2809869b116827a9c78f76c9cc110b6491a7e663
Evgen_Chernovol <ievgenchernovol@gmail.com>
2018-11-18 15:35:30 +0200

    boot

---
+++ lesson18_bootstrap/src/js/script.js
@@ -0,0 +1,34 @@
+let userName = prompt('What is your name?');
+
+console.log(`Hello ${userName}`);
+
+const arr = [];
+
+/* for (let i = 0; i <= 100; i++) {
+     arr.push(String (i));
+}
+console.log (arr);
+
+let newArr = [];
+
+arr.forEach(el, idx, arr) => {
+   if (idx >= arr.length - 11){
+   newArr.push(el);
+ 
+  }};
+  
+ console.log (newArr);*/
+/*-------------------------------------*/
+ /*let i =1 ;
+ while(i <= 100){
+ 	arr.push(String(i));
+ 	i++;
+ }*/
+ /*---------------------------------*/
+let random;
+
+ do{
+ random = Math.floor((Math.random() * 10) + 50);
+} while (random !== 7);
+
+console.let(Cheers!!);
